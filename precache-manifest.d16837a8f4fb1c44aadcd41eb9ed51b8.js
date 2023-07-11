2153bc86d7aa5f8fa2278786c9f872bdab90cbbb
himanshu2312 <himanshu2312gupta@gmail.com>
2023-07-11 16:25:26 +0530

    Updates

---
+++ precache-manifest.d16837a8f4fb1c44aadcd41eb9ed51b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "41214ab8ea32cb60cc57",
+    "url": "/Text-Utils/static/css/main.003d4771.chunk.css"
+  },
+  {
+    "revision": "41214ab8ea32cb60cc57",
+    "url": "/Text-Utils/static/js/main.41214ab8.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/Text-Utils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/Text-Utils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "af7ba7e700011b21addd",
+    "url": "/Text-Utils/static/js/runtime~main.af7ba7e7.js"
+  },
+  {
+    "revision": "15952f70898df8dbf8b5b664b8a77dcc",
+    "url": "/Text-Utils/index.html"
+  }
+];
